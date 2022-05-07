5c3c798195a651e599d78d36b3aad4e1f461579a
bpbirch <brendanpbirch@gmail.com>
2022-05-07 11:01:37 -0500

    package.json correctly configured to not need to use /dist/ in import path

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+export {log} from './commands/lib';
+export {SimbaConfig} from './commands/lib';
+export {
+    promisifiedReadFile,
+    walkDirForContracts,
+} from './commands/lib';
+export {
+    chooseApplicationFromList,
+    chooseOrganisationFromList,
+    getApp,
+    getBlockchains,
+    getStorages,
+    primaryConstructorRequiresArgs,
+    primaryConstructorInputs,
+    writeAndReturnASTSourceAndCompiler,
+} from './commands/lib';
