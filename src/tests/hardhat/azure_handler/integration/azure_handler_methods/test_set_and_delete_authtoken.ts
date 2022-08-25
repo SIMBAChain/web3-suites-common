63b5642eec8e4c0ff794eaa166e7befdbc5f43e1
bpbirch <brendanpbirch@gmail.com>
2022-08-25 17:57:36 -0500

    tests uncommented

--- src/tests/hardhat/azure_handler/integration/azure_handler_methods/test_set_and_delete_authtoken.ts
+++ src/tests/hardhat/azure_handler/integration/azure_handler_methods/test_set_and_delete_authtoken.ts
@@ -1,50 +1,50 @@
-// import {
-//     SimbaConfig,
-// } from "../../../../../commands/lib/config";
-// import {
-//     AzureHandler,
-// } from "../../../../../commands/lib";
-// import { expect } from 'chai';
-// import 'mocha';
+import {
+    SimbaConfig,
+} from "../../../../../commands/lib/config";
+import {
+    AzureHandler,
+} from "../../../../../commands/lib";
+import { expect } from 'chai';
+import 'mocha';
 
-// describe('tests performLogin with non-interactive login', () => {
-//     it('authtoken should be present after calling performLogin', async () => {
-//         // original settings
-//         const originalSimbaJson = SimbaConfig.ProjectConfigStore.all;
-//         const originalAuthConfig = SimbaConfig.ConfigStore.all;
-//         const az = new AzureHandler();
+describe('tests performLogin with non-interactive login', () => {
+    it('authtoken should be present after calling performLogin', async () => {
+        // original settings
+        const originalSimbaJson = SimbaConfig.ProjectConfigStore.all;
+        const originalAuthConfig = SimbaConfig.ConfigStore.all;
+        const az = new AzureHandler();
 
-//         // function
-//         az.logout();
-//         const currentAuthToken = az.getConfig("SIMBAAUTH");
-//         let isLoggedIn = az.isLoggedIn();
+        // function
+        az.logout();
+        const currentAuthToken = az.getConfig("SIMBAAUTH");
+        let isLoggedIn = az.isLoggedIn();
         
-//         // prior
-//         expect(currentAuthToken).to.not.exist;
-//         expect(isLoggedIn).to.equal(false);
+        // prior
+        expect(currentAuthToken).to.not.exist;
+        expect(isLoggedIn).to.equal(false);
 
-//         // function
-//         await az.performLogin(false);
-//         let newAuthToken = az.getConfig("SIMBAAUTH");
+        // function
+        await az.performLogin(false);
+        let newAuthToken = az.getConfig("SIMBAAUTH");
         
-//         // posterior
-//         expect(newAuthToken).to.exist;
-//         isLoggedIn = az.isLoggedIn();
-//         expect(isLoggedIn).to.equal(true);
+        // posterior
+        expect(newAuthToken).to.exist;
+        isLoggedIn = az.isLoggedIn();
+        expect(isLoggedIn).to.equal(true);
 
-//         // function
-//         az.logout();
-//         newAuthToken = az.getConfig("SIMBAAUTH");
+        // function
+        az.logout();
+        newAuthToken = az.getConfig("SIMBAAUTH");
 
-//         // posterior
-//         expect(newAuthToken).to.not.exist;
-//         isLoggedIn = az.isLoggedIn();
-//         expect(isLoggedIn).to.equal(false);
+        // posterior
+        expect(newAuthToken).to.not.exist;
+        isLoggedIn = az.isLoggedIn();
+        expect(isLoggedIn).to.equal(false);
         
-//         // resetting
-//         SimbaConfig.ProjectConfigStore.clear();
-//         SimbaConfig.ProjectConfigStore.set(originalSimbaJson);
-//         SimbaConfig.ConfigStore.clear();
-//         SimbaConfig.ConfigStore.set(originalAuthConfig);
-//     }).timeout(10000);
-// });
+        // resetting
+        SimbaConfig.ProjectConfigStore.clear();
+        SimbaConfig.ProjectConfigStore.set(originalSimbaJson);
+        SimbaConfig.ConfigStore.clear();
+        SimbaConfig.ConfigStore.set(originalAuthConfig);
+    }).timeout(10000);
+});
