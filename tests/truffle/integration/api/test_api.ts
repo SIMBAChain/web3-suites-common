d828af414d92ec4b03217779f5e61b1396733f3b
bpbirch <brendanpbirch@gmail.com>
2023-03-17 11:47:48 -0600

    running tests with separate commands

--- tests/truffle/integration/api/test_api.ts
+++ tests/truffle/integration/api/test_api.ts
@@ -1,27 +1,27 @@
-// import {
-//     SimbaConfig,
-// } from "../../../../src/commands/lib/config";
-// import {
-//     getBlockchains,
-//     getStorages,
-// } from "../../../../src/commands/lib";
-// import { expect } from 'chai';
-// import 'mocha';
+import {
+    SimbaConfig,
+} from "../../../../src/commands/lib/config";
+import {
+    getBlockchains,
+    getStorages,
+} from "../../../../src/commands/lib";
+import { expect } from 'chai';
+import 'mocha';
 
-// describe('test getBlockchains', () => {
-//     it('result should exist', async () => {
-//         const authStore = await SimbaConfig.authStore();
-//         await authStore!.performLogin(false);
-//         const blockChains = await getBlockchains(new SimbaConfig());
-//         expect(blockChains).to.exist;
-//     }).timeout(60000);
-// });
+describe('test getBlockchains', () => {
+    it('result should exist', async () => {
+        const authStore = await SimbaConfig.authStore();
+        await authStore!.performLogin(false);
+        const blockChains = await getBlockchains(new SimbaConfig());
+        expect(blockChains).to.exist;
+    }).timeout(60000);
+});
 
-// describe('test getStorages', () => {
-//     it('result should exist', async () => {
-//         const authStore = await SimbaConfig.authStore();
-//         await authStore!.performLogin(false);
-//         const storages = await getStorages(new SimbaConfig())
-//         expect(storages).to.exist;
-//     }).timeout(30000);
-// });
+describe('test getStorages', () => {
+    it('result should exist', async () => {
+        const authStore = await SimbaConfig.authStore();
+        await authStore!.performLogin(false);
+        const storages = await getStorages(new SimbaConfig())
+        expect(storages).to.exist;
+    }).timeout(30000);
+});
--- tests/truffle/integration/contract/test_list_contracts.ts
+++ tests/truffle/integration/contract/test_list_contracts.ts
@@ -1,42 +1,42 @@
-// import {
-//     SimbaConfig,
-// } from "../../../../src/commands/lib/config";
-// import { expect } from 'chai';
-// import 'mocha';
-// import {
-//     allContractsStubFunc,
-// } from "../../../tests_setup";
-// const contractLib = require("../../../../src/commands/contract/list");
-// import {
-//     allContracts,
-// } from "../../../../src/commands/contract/list";
-// import sinon from "sinon";
+import {
+    SimbaConfig,
+} from "../../../../src/commands/lib/config";
+import { expect } from 'chai';
+import 'mocha';
+import {
+    allContractsStubFunc,
+} from "../../../tests_setup";
+const contractLib = require("../../../../src/commands/contract/list");
+import {
+    allContracts,
+} from "../../../../src/commands/contract/list";
+import sinon from "sinon";
 
 
-// describe('testing gathering all contracts', () => {
-//     it('first entry of results should contain "id" and "name" properties', async () => {
-//         // define our stub
-//         const fakeContractData = await allContractsStubFunc("demo")
-//         const sandbox = sinon.createSandbox();
-//         sandbox.stub(contractLib, "allContracts").callsFake(() => fakeContractData);
-//         const contracts: any = await allContracts();
-//         // just a couple lines to make sure we're getting back the right kind of info
-//         expect("id" in contracts[0]).to.equal(true);
-//         expect("name" in contracts[0]).to.equal(true);
-//         expect("version" in contracts[0]).to.equal(true);
-//         expect("created_on" in contracts[0]).to.equal(true);
-//         expect("updated_on" in contracts[0]).to.equal(true);
-//         expect("code" in contracts[0]).to.equal(true);
-//         expect("language" in contracts[0]).to.equal(true);
-//         expect("metadata" in contracts[0]).to.equal(true);
-//         expect("err" in contracts[0]).to.equal(true);
-//         expect("mode" in contracts[0]).to.equal(true);
-//         expect("model" in contracts[0]).to.equal(true);
-//         expect("service_args" in contracts[0]).to.equal(true);
-//         expect("asset_type" in contracts[0]).to.equal(true);
-//         expect("organisation" in contracts[0]).to.equal(true);
-//         expect("designset" in contracts[0]).to.equal(true);
+describe('testing gathering all contracts', () => {
+    it('first entry of results should contain "id" and "name" properties', async () => {
+        // define our stub
+        const fakeContractData = await allContractsStubFunc("demo")
+        const sandbox = sinon.createSandbox();
+        sandbox.stub(contractLib, "allContracts").callsFake(() => fakeContractData);
+        const contracts: any = await allContracts();
+        // just a couple lines to make sure we're getting back the right kind of info
+        expect("id" in contracts[0]).to.equal(true);
+        expect("name" in contracts[0]).to.equal(true);
+        expect("version" in contracts[0]).to.equal(true);
+        expect("created_on" in contracts[0]).to.equal(true);
+        expect("updated_on" in contracts[0]).to.equal(true);
+        expect("code" in contracts[0]).to.equal(true);
+        expect("language" in contracts[0]).to.equal(true);
+        expect("metadata" in contracts[0]).to.equal(true);
+        expect("err" in contracts[0]).to.equal(true);
+        expect("mode" in contracts[0]).to.equal(true);
+        expect("model" in contracts[0]).to.equal(true);
+        expect("service_args" in contracts[0]).to.equal(true);
+        expect("asset_type" in contracts[0]).to.equal(true);
+        expect("organisation" in contracts[0]).to.equal(true);
+        expect("designset" in contracts[0]).to.equal(true);
 
-//         sandbox.restore();
-//     }).timeout(1000);
-// });
+        sandbox.restore();
+    }).timeout(1000);
+});
